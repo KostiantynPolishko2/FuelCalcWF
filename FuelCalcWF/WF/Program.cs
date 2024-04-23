82a20d645cc33c99408f1437a7f828dc14919caa
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-04-23 18:12:05 +0300

    issue v1.0.0 (#5)
    * WF FuelCalc 1st part (#4)
    
    * 1 Init Solution FuelCalculation
    
    * Created WinForms Calc
    
    ---------
    
    Co-authored-by: edinson17m <MXrza_bh74@student.itstep.org>
    
    * 2 Created project FuelCalcLibrary
    
    FuelCalcLibrary has class FuelCalc.cs that has logic of fuel cost depend from distance and consumation.
    
    * 3 Created WF class FuelCalc2.cs
    
    FuelCalc2.cs extended FuelCalc.cs in order to anticipate uncorrect input inside fields.
    
    ---------
    
    Co-authored-by: edinson17m <MXrza_bh74@student.itstep.org>

--- FuelCalcWF/WF/Program.cs
+++ FuelCalcWF/WF/Program.cs
@@ -1,4 +1,4 @@
-namespace WF
+namespace WF.Views
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace WF
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FuelCalc());
         }
     }
 }
