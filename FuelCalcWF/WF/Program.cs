bdbd8ab4c07c427714953705fa2fc6d0fea067d1
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-04-23 17:02:35 +0300

    Revert "Created WinForms Calc (#1)"
    This reverts commit fde8d705cc083138c0a904d5d0298371de44e3f1.

--- FuelCalcWF/WF/Program.cs
+++ FuelCalcWF/WF/Program.cs
@@ -11,7 +11,7 @@ namespace WF
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FuelCalc());
+            Application.Run(new Form1());
         }
     }
 }
