25a137e29c0a67f93746e700571a73bca0c85877
edinson17m <MXrza_bh74@student.itstep.org>
2024-04-25 11:53:27 +0300

    2 createt Sql MSSQL

---
+++ FuelCalcWF/Bd/Models/Auto_db2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bd.Models
+{
+    public partial class Auto_db
+    {
+        public override string ToString()
+        {
+            return $"{mark},{model},{engine_power},{fuel_consumption}";
+        }
+    }
+}
---
+++ FuelCalcWF/Bd/Program.cs
@@ -0,0 +1,23 @@
+﻿using Bd.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            db_aa7fda_edmond171195001Context db = new db_aa7fda_edmond171195001Context();
+            List<Auto_db> autos = db.Auto_dbs.ToList();
+
+            foreach (Auto_db auto_db in autos)
+            {
+
+                Console.WriteLine(auto_db);
+            }
+        }
+    }
+}
